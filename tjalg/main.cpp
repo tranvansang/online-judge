80dc26821fe1f439af839039f497eb13d81bd558
Tran Sang <me@transa.ng>
2022-05-11 06:49:24 +0900

    tarjan new graph structure

--- tjalg/main.cpp
+++ tjalg/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define PII pair<int,int>
 #define VI vector<int>
+#define VVI vector<VI>
 #define For(i,a,b) for(auto i = (a); i < (b); i++)
 #define rep(i,n) For(i,0,(n))
 #define fi first
@@ -13,16 +14,16 @@ using namespace std;
 #define ll long long
 
 #define NMAX 10000
-VI nxt, adj;
+VVI g;
 stack<int> s;
-int start[NMAX], low[NMAX], num[NMAX], comp[NMAX];
+int low[NMAX], num[NMAX], comp[NMAX];
 int cnt(0), numCnt(0);
 
 void dfs(int x) {
 	s.push(x);
 	num[x] = low[x] = ++numCnt;
-	for(int k = start[x]; k != -1; k = nxt[k]) {
-		int y = adj[k];
+	trav(it, g[x]) {
+		int y = *it;
 		if (!comp[y]) {
 			if (!num[y]) {
 				dfs(y);
@@ -42,15 +43,12 @@ int main(){
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	int n, m; cin >> n >> m;
-	fill_n(start, n, -1);
+	g = VVI(n, VI());
 	rep(i, m) {
 		int u, v; cin >> u >> v;
-		u--; v--;
-		nxt.pb(start[u]);
-		adj.pb(v);
-		start[u] = nxt.size() - 1;
+		g[--u].pb(--v);
 	}
-	rep(i, n) if (!num[i]) dfs(0);
+	rep(i, n) if (!num[i]) dfs(i);
 	cout << cnt;
 
 	return 0;
