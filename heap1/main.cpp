8b3afc2ae9f002b05a195af1ca20da9553edccd1
Tran Sang <me@transa.ng>
2022-05-10 09:46:28 +0900

    heap manual

--- heap1/main.cpp
+++ heap1/main.cpp
@@ -12,23 +12,48 @@ using namespace std;
 #define trav(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
 #define ll long long
 
+#define NMAX 20000
+
+int top, heap[NMAX + 1];
+
+void push(int a) {
+	top++;
+	heap[top] = a;
+	for(int i = top; i / 2 > 0; i /= 2) {
+		int p = i / 2;
+		if (heap[p] <= heap[i]) break;
+		int t = heap[p]; heap[p] = heap[i]; heap[i] = t;
+	}
+}
+
+int pop() {
+	int val = heap[1];
+	heap[1] = heap[top]; top--;
+	for(int i = 1; i * 2 <= top; ) {
+		int c = i * 2;
+		if (c < top && heap[c + 1] < heap[c]) c++;
+		if (heap[i] <= heap[c]) break;
+		int t = heap[i]; heap[i] = heap[c]; heap[c] = t;
+		i = c;
+	}
+	return val;
+}
+
 int main(){
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	int ntest; cin >> ntest;
-	rep(t, ntest) {
+	rep(_, ntest) {
+		top = 0;
 		int n; cin >> n;
-		priority_queue<int, VI, greater<int>> q;
 		rep(i, n) {
-			int a; cin >> a;
-			q.push(a);
+			int a; cin >> a; push(a);
 		}
 		ll sum(0);
 		rep(i, n - 1) {
-			int t1 = q.top(); q.pop();
-			int t2 = q.top(); q.pop();
-			sum += t1 + t2;
-			q.push(t1 + t2);
+			int t = pop() + pop();
+			sum += t;
+			push(t);
 		}
 		cout << sum << endl;
 	}
